3c53ef12dfe8d100338a172668133acfe2f5deaf
peterihimire <peterihimire@gmail.com>
2023-09-08 23:30:35 +0100

    Updated my migration file, and working on auth register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('api/ecommerce/v1/auth/')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  register() {
+  register(@Body() dto: AuthDto) {
+    console.log('This is dto...', dto);
     return this.authService.register();
   }
 
