6fa88695ff7f264ff044cf1c71fbd8b9bb8ee1f9
peterihimire <peterihimire@gmail.com>
2023-09-10 12:48:25 +0100

    Working on the user authentication and  autorization

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 // import { AuthDto } from './dto';
 
-@Controller('api/ecommerce/v1/user/')
-export class AuthController {
+@UseGuards(JwtGuard) //general route
+@Controller('api/ecommerce/v1/users/')
+export class UserController {
   constructor(private userService: UserService) {}
 
+  // @UseGuards(JwtGuard) //individual route
   @Get('dashboard')
-  register() {
-    // console.log('This is dto...', dto);
-    return this.userService.dashboard();
+  dashboard(@GetUser() user: User) {
+    console.log('This is user object...', user);
+    // return this.userService.dashboard();
+    return user;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,44 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+// import { PrismaService } from 'src/prisma/prisma.service';
 // import { AuthDto } from './dto';
 // import * as argon from 'argon2';
 // import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-import { JwtService } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
+// import { JwtService } from '@nestjs/jwt';
+// import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
-  constructor(
-    private prisma: PrismaService,
-    private jwt: JwtService,
-    private config: ConfigService,
-  ) {}
+  constructor() {}
 
   async dashboard() {
     try {
+      return {
+        msg: 'Dashboard success',
+      };
     } catch (error) {
       throw error;
     }
-    // return { msg: 'Registration success' };
-  }
-
-  async signToken(
-    acctId: string,
-    email: string,
-  ): Promise<{ access_token: string }> {
-    const payload = {
-      sub: acctId,
-      email,
-    };
-    const secret = this.config.get('JWT_SECRET');
-
-    const token = await this.jwt.signAsync(payload, {
-      expiresIn: '10m',
-      secret: secret,
-    });
-
-    return {
-      access_token: token,
-    };
   }
 }
